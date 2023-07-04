c8c5f3814b4769f45e508aa4addb873d81c4f942
Csaba Miskó <107340437+Birbseye@users.noreply.github.com>
2023-07-04 11:26:16 +0200

    init

---
+++ README.md
@@ -0,0 +1,44 @@
+**Enum game**
+
+You are in a game development team where the goal is to create a game that can be easily expanded. The team decides to change the values ​​of characters in the game using enums.
+
+There is a basic character class, where the basic settings are as follows:
+- name = name?
+- healthPoint = 50
+- damage = 30
+- speed = 20
+- armor = 0
+- type = type?
+
+When instantiating, the default values ​​are changed based on the type.
+
+You need to create modifier values ​​for the following characters:
+
+ORC:
+- +10 damage
+- -5 speed
+- +10 health points
+- +5 armor
+
+KNIGHT:
+- +5 damage
+- +20 speed
+- -5 health points
+- +20 armor
+
+WIZARD
+- -5 damage
+- +0 speed
+- +5 health points
+- +0 armor
+
+ASSASSIN:
+- +15 damage
+- +10 speed
+- -5 health points
+- +0 armor
+
+... and there is a mysterious character who gets a random type on each instantiation.
+You can test in the Game main class with a while loop for example...
+
+These modifiers cannot be changed externally.
---
+++ src/test/java/EnumGameTest.java
@@ -0,0 +1,59 @@
+import org.enumgame.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+class EnumGameTest {
+
+     Orc orc;
+     Knight knight;
+     Wizard wizard;
+     Assassin assassin;
+
+    @BeforeEach
+    void setup() {
+        orc = new Orc("HARGH");
+        knight = new Knight("Adam");
+        wizard = new Wizard("Merlin");
+        assassin = new Assassin("Ezio");
+    }
+
+    @Test
+    void test_orc_values() {
+        assertEquals(40, orc.getDamage());
+        assertEquals(60, orc.getHealthPoint());
+        assertEquals(15, orc.getSpeed());
+        assertEquals(5, orc.getArmor());
+        assertEquals(Type.ORC, orc.getType());
+    }
+
+    @Test
+    void test_knight_values() {
+        assertEquals(35, knight.getDamage());
+        assertEquals(45, knight.getHealthPoint());
+        assertEquals(40, knight.getSpeed());
+        assertEquals(20, knight.getArmor());
+        assertEquals(Type.KNIGHT, knight.getType());
+    }
+
+    @Test
+    void test_wizard_values() {
+        assertEquals(25, wizard.getDamage());
+        assertEquals(55, wizard.getHealthPoint());
+        assertEquals(20, wizard.getSpeed());
+        assertEquals(0, wizard.getArmor());
+        assertEquals(Type.WIZARD, wizard.getType());
+    }
+
+    @Test
+    void test_assassin_values() {
+        assertEquals(45, assassin.getDamage());
+        assertEquals(45, assassin.getHealthPoint());
+        assertEquals(30, assassin.getSpeed());
+        assertEquals(0, assassin.getArmor());
+        assertEquals(Type.ASSASSIN, assassin.getType());
+    }
+
+}
